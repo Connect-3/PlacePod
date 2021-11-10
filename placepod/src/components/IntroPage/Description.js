4a10f54afa57822c166249675e1da7fba4ae94ec
Sudeep Srivastava <sudeepsrivastava244@gmail.com>
2021-11-10 16:31:10 +0530

    LeftSide Updated

---
+++ placepod/src/components/IntroPage/Description.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './IntroPage.css'
+const Description = () => {
+    return (
+        <div className ="description-body">
+        <p>PlacePod </p>
+        <p>A Place for all your needs</p>
+        </div>
+    )
+}
+
+export default Description
