31af003427074e4612eab8eaaad88e4b1d6e493b
Sudeep Srivastava <sudeepsrivastava244@gmail.com>
2021-11-13 13:56:41 +0530

    check

--- placepod/src/components/IntroPage/IntroPage_BASE_1813.js
+++ placepod/src/components/IntroPage/IntroPage_BASE_1813.js
@@ -13,17 +13,4 @@ const IntroPage = () => {
     </div>
   );
 };
-import React from "react";
-import Login from "./Login";
-import Description from "./Description";
-const IntroPage = ()=>{
-    return (
-        <div className="ui grid">
-            <div className="six wide column"><Description/></div>
-            <div className="ten wide column"><Login /></div>
-            <div></div>
-        </div>
-    )
-}
-
 export default IntroPage;
