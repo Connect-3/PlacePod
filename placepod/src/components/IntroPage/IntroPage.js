f0136520102df91188e30d7111ae60d3facc93cb
Sudeep Srivastava <sudeepsrivastava244@gmail.com>
2021-11-13 13:34:21 +0530

    Completed leftside

--- placepod/src/components/IntroPage/IntroPage.js
+++ placepod/src/components/IntroPage/IntroPage.js
@@ -1,18 +1,3 @@
-import React from 'react';
-import Login from './Login';
-import './IntroPage.css';
-
-const IntroPage = () => {
-  return (
-    <div className="ui grid container">
-      <div className="six wide column">Hello there</div>
-      <div className="ten wide column">
-        <Login />
-      </div>
-      <div></div>
-    </div>
-  );
-};
 import React from "react";
 import Login from "./Login";
 import Description from "./Description";
